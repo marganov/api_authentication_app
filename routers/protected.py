f21da8df8ef12291c69a2fdf899f92f624c93448
margan <marganovalexey@gmail.com>
2025-11-08 10:51:37 +0300

    modified:   routers/protected.py

--- routers/protected.py
+++ routers/protected.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, HTTPException, Depends
-from config.jwt_utils import oauth2_scheme, verify_token
+from config.jwt_utils import verify_token
 
 router = APIRouter()
 
 @router.get("/protected_resource/")
-def protected_resource(token=Depends(oauth2_scheme)):
+def protected_resource(token):
     username = verify_token(token)
     return {"message": f"Hello {username}! Access granted."}
